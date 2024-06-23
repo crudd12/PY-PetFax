6f2aa6b3fc0cd215828b2bc740f721e486b26177
crudd12 <crudd12@gmail.com>
2024-06-23 14:13:17 -0700

    added POST and connections to push form input to postgres

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask 
+import os
+from flask_migrate import Migrate
 
 # factory
 def create_app(): 
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+    
+    from . import models
+    models.db.init_app(app)     
+    
+    migrate = Migrate(app, models.db)   
 
     @app.route('/')
     def hello(): 
@@ -10,5 +20,8 @@ def create_app():
     
     from . import pet
     app.register_blueprint(pet.bp)
+    
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     return app
--- petfax/pet.py
+++ petfax/pet.py
@@ -15,6 +15,3 @@ def show(pet_id):
         if pet['pet_id'] == pet_id:
             return render_template('show.html', pet=pet)
         
-@bp.route('/facts/new')
-def fact():
-    return render_template('fact.html')
